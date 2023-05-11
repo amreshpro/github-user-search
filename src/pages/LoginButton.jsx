08e615d71a35cd01528ece8bc3f1c34d8195759a
Amresh Maurya <amresh.terminal@gmail.com>
2023-05-11 11:15:40 +0530

    remove semicolon and improve login ui

--- src/pages/LoginButton.jsx
+++ src/pages/LoginButton.jsx
@@ -6,11 +6,11 @@ const LoginButton = () => {
   const { loginWithRedirect ,isLoading,isAuthenticated} = useAuth0();
 
 
-  return <div className="flex flex-col justify-center items-center">
-<div className="git">
-<img src="/github.png" alt="git-logo" className="rounded-full"/>
+  return <div className="flex flex-col m-1 justify-center items-center">
+<div className="git p-1 px-2">
+<img src="/github.png" alt="git-logo" className="rounded-full object-fill w-56"/>
 </div>
-  <button className=" flex px-2 py-1 text-3xl bg-primary-800 rounded-lg text-primary-200 text-center" onClick={loginWithRedirect}> <FcGoogle className="px-2 py-1 text-5xl"/>  <strong className="mt-1">Log In </strong></button>;
+  <button className="mt-3  flex px-2 py-1 text-2xl bg-primary-800 rounded-lg text-primary-200 text-center pr-4" onClick={loginWithRedirect}> <FcGoogle className="px-2 py-1 text-5xl"/>  <span className="mt-1">Log In </span></button>
   </div>
 };
 
